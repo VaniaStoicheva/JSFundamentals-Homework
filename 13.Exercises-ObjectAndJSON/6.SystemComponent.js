8be6fd413d867321bc3954e533b2e2a13e518df1
VaniaStoicheva <cecostoi4ev@abv.bg>
2017-06-15 08:08:57 +0300

    Change 13.6

--- 13.Exercises-ObjectAndJSON/6.SystemComponent.js
+++ 13.Exercises-ObjectAndJSON/6.SystemComponent.js
@@ -16,7 +16,7 @@ function component(strArr) {
         componentMap.get(name).get(component).push(subcomponent);
     }
 
-    let sortedMap=[...componentMap.entries()].sort(sortByComponent);
+    let sortedMap=[...componentMap.entries()].sort(compareSystems);
     for(let [name,componentMap] of sortedMap){
         console.log(name);
         let sortedComponentMap=[...componentMap.entries()].sort(sorter);
